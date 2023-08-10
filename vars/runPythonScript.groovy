f1e48eb6a1e6012892f2d49ae1d37f08ded4a614
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-11 00:51:26 +0200

    changed the name of the bandit function

--- vars/runPythonScript.groovy
+++
@@ -1,5 +0,0 @@
-def generateBanditReport(String jsonReportPath = 'reports/bandit-results.json') {
-    def scriptPath = libraryResource('bandit/html_generator.py')
-    sh "cp ${scriptPath} ."
-    sh "python html_generator.py ${jsonReportPath}"
-}
