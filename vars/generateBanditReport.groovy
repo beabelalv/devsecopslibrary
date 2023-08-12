897f68a5c71e3c7cad8b9efb93469b6b63c00b2a
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-12 12:32:27 +0200

    Added Debug Line

--- vars/generateBanditReport.groovy
+++ vars/generateBanditReport.groovy
@@ -1,4 +1,5 @@
 def call(String jsonReportPath) {
     def scriptPath = libraryResource('bandit/html_generator.py')
+    echo "Script Path: ${scriptPath}"
     sh "python ${scriptPath} ${jsonReportPath}"
 }
