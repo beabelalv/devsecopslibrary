85333868f94df9c6f386299851feefb7906eeac4
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-12 12:12:37 +0200

    Removed file cp

--- vars/generateBanditReport.groovy
+++ vars/generateBanditReport.groovy
@@ -1,5 +1,4 @@
 def call(String jsonReportPath) {
     def scriptPath = libraryResource('bandit/html_generator.py')
-    sh "cp ${scriptPath} ."
     sh "python html_generator.py ${jsonReportPath}"
 }
