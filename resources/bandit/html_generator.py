fed094418cae76229e3d73e69f2139030158b5bd
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-12 12:18:20 +0200

    Added arguments support

--- resources/bandit/html_generator.py
+++ resources/bandit/html_generator.py
@@ -1,9 +1,13 @@
 from jinja2 import Environment, FileSystemLoader
 from parser import parse_json, load_and_parse
 from graphics_generator import generate_all_plots  # Import the new function
+import sys  # Import sys to work with command-line arguments
 
-# Parse JSON data to create df
-df = load_and_parse('./reports/bandit-results.json')
+# Check if a command-line argument was passed, else use the default path
+json_report_path = sys.argv[1] if len(sys.argv) > 1 else './reports/bandit-results.json'
+
+# Parse JSON data to create df using the variable json_report_path
+df = load_and_parse(json_report_path)
 
 # Generate plots
 generate_all_plots()  # Call the new function
