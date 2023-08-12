8f59b70fd268eb8de6cb7829b93952b6ad478ad9
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-12 12:12:06 +0200

    Changed the way the library is called

--- vars/generateBanditReport.groovy
+++ vars/generateBanditReport.groovy
@@ -1,6 +1,5 @@
 def call(String jsonReportPath) {
-    sh "echo Hello World"
-    //def scriptPath = libraryResource('bandit/html_generator.py')
-    //sh "cp ${scriptPath} ."
-    //sh "python html_generator.py ${jsonReportPath}"
+    def scriptPath = libraryResource('bandit/html_generator.py')
+    sh "cp ${scriptPath} ."
+    sh "python html_generator.py ${jsonReportPath}"
 }
