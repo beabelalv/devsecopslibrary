74f7d758c9ae3d0131d2475093ad9acf50e3751b
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-12 12:02:47 +0200

    Changed Library Structure

--- vars/generateBanditReport.groovy
+++ vars/generateBanditReport.groovy
@@ -1,4 +1,4 @@
-def generateBanditReport(String jsonReportPath = 'reports/bandit-results.json') {
+def call(String jsonReportPath = 'reports/bandit-results.json') {
     def scriptPath = libraryResource('bandit/html_generator.py')
     sh "cp ${scriptPath} ."
     sh "python html_generator.py ${jsonReportPath}"
