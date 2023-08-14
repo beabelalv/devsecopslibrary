0841518ce3477f3a57c3f39e11308f8682e87cdb
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-14 12:23:04 +0200

    Imported functions in the same file

--- resources/bandit/html_generator.py
+++ resources/bandit/html_generator.py
@@ -1,6 +1,26 @@
 from jinja2 import Environment, FileSystemLoader
-from .parser import parse_json, load_and_parse
+import pandas as pd
+#from .parser import parse_json, load_and_parse
 from .graphics_generator import generate_all_plots  # Import the new function
+
+#FUNCTIONS#
+def parse_json(data):
+    # Convert to DataFrame
+    df = pd.json_normalize(data['results'], 
+                           meta=['filename'], errors='ignore')
+
+    return df
+
+def load_and_parse(file_path):
+    # Load JSON data
+    with open(file_path) as f:
+        data = json.load(f)
+
+    # Parse data into DataFrame
+    df = parse_json(data)
+    #print(df.head())
+    return df
+
 
 # Parse JSON data to create df
 df = load_and_parse('./reports/bandit-results.json')
