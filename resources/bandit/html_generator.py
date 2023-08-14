ac5be7122400f005042997815bcfbd7ad4dbe103
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-14 14:10:06 +0200

    html generator template modified

--- resources/bandit/html_generator.py
+++ resources/bandit/html_generator.py
@@ -1,3 +1,5 @@
+# Modifying the existing Python code to include the new plots
+
 import argparse
 import json
 import os
@@ -24,7 +26,6 @@ def get_image_as_data_url(image_path):
         encoded_image = base64.b64encode(image_file.read()).decode()
     return f"data:image/png;base64,{encoded_image}"
 
-# Functions
 def parse_json(data):
     df = pd.json_normalize(data['results'], meta=['filename'], errors='ignore')
     return df
@@ -62,10 +63,37 @@ def generate_file_plot(df):
     plt.tight_layout()
     plt.savefig(os.path.join(images_path, 'file_counts.png'), dpi=300)
 
+# New function to generate confidence level plot (Plot 4)
+def generate_confidence_plot(df):
+    confidence_counts = df['issue_confidence'].value_counts()
+    plt.figure(figsize=(20, 12))
+    plt.rcParams.update({'font.size': 18})
+    sns.barplot(x=confidence_counts.index, y=confidence_counts.values, palette='viridis')
+    plt.title('Distribution of Confidence Levels')
+    plt.xlabel('Confidence Level')
+    plt.ylabel('Number of Issues')
+    plt.tight_layout()
+    plt.savefig(os.path.join(images_path, 'confidence_counts.png'), dpi=300)
+
+# New function to generate CWE ID plot (Plot 7)
+def generate_cwe_plot(df):
+    cwe_counts = df['issue_cwe.id'].value_counts().head(10) # Taking top 10 CWE IDs
+    plt.figure(figsize=(20, 12))
+    plt.rcParams.update({'font.size': 18})
+    sns.barplot(x=cwe_counts.index, y=cwe_counts.values, palette='coolwarm')
+    plt.title('Distribution of CWE IDs (Top 10)')
+    plt.xlabel('CWE ID')
+    plt.ylabel('Number of Issues')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig(os.path.join(images_path, 'cwe_counts.png'), dpi=300)
+
 def generate_all_plots(file_path):
     df = load_and_parse(file_path)
     generate_severity_plot(df)
     generate_file_plot(df)
+    generate_confidence_plot(df) # Generating Plot 4
+    generate_cwe_plot(df)       # Generating Plot 7
 
 # Main
 df = load_and_parse(file_path)
@@ -74,6 +102,8 @@ generate_all_plots(file_path)
 # Convert the images to data URLs
 severity_plot_data_url = get_image_as_data_url(os.path.join(images_path, 'severity_counts.png'))
 file_plot_data_url = get_image_as_data_url(os.path.join(images_path, 'file_counts.png'))
+confidence_plot_data_url = get_image_as_data_url(os.path.join(images_path, 'confidence_counts.png')) # Data URL for Plot 4
+cwe_plot_data_url = get_image_as_data_url(os.path.join(images_path, 'cwe_counts.png'))             # Data URL for Plot 7
 
 env = Environment(loader=FileSystemLoader('./'))
 template = env.get_template(template_path)
@@ -82,7 +112,9 @@ print("Rendering template...")
 html_content = template.render(
     data=df,
     severity_plot=severity_plot_data_url,
-    file_plot=file_plot_data_url
+    file_plot=file_plot_data_url,
+    confidence_plot=confidence_plot_data_url, # Including Plot 4 in the rendered HTML
+    cwe_plot=cwe_plot_data_url                # Including Plot 7 in the rendered HTML
 )
 
 print("Writing HTML content to file...")
