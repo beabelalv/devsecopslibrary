04669d1bff32534475f37a37d92e4115f63c3694
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-14 11:32:53 +0200

    Modified library

--- vars/generateBanditReport.groovy
+++ vars/generateBanditReport.groovy
@@ -1,6 +1,6 @@
 def call(String jsonReportPath) {
     // Load the loadScript.groovy file
-    def loadScript = load 'path/to/loadScript.groovy'
+    def loadScript = load './loadScript.groovy'
 
     // Use the call function to run the specific script with the JSON report path as an argument
     loadScript.call('bandit/html_generator.py', jsonReportPath)
