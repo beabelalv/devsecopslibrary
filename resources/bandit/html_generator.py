75c5266cb56b432472098e806b3b59350f425974
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-14 12:33:21 +0200

    Edited Library

--- resources/bandit/html_generator.py
+++ resources/bandit/html_generator.py
@@ -1,9 +1,11 @@
 from jinja2 import Environment, FileSystemLoader
 import pandas as pd
-#from .parser import parse_json, load_and_parse
-from graphics_generator import generate_all_plots  # Import the new function
+import matplotlib.pyplot as plt
+import seaborn as sns
+from parser import load_and_parse
 
-#FUNCTIONS#
+
+#Functions:#
 def parse_json(data):
     # Convert to DataFrame
     df = pd.json_normalize(data['results'], 
@@ -21,6 +23,52 @@ def load_and_parse(file_path):
     #print(df.head())
     return df
 
+#Graphics:
+
+def generate_severity_plot(df):
+    # Count the number of issues per severity level
+    severity_counts = df['issue_severity'].value_counts()
+
+    # Define color palette based on severity
+    severity_palette = {
+        'LOW': "#FFEB3B",
+        'MEDIUM': "#FF9800",
+        'HIGH': "#F44336"
+    }
+
+    # Plot the number of issues per severity level
+    plt.figure(figsize=(20, 12))  # Make the image larger
+    plt.rcParams.update({'font.size': 18})  # Increase font size
+    bars = plt.bar(severity_counts.index, severity_counts.values, color=[severity_palette[severity] for severity in severity_counts.index])
+    plt.title('Number of Issues per Severity Level')
+    plt.xlabel('Severity Level')
+    plt.ylabel('Number of Issues')
+    plt.tight_layout()  # This will prevent the labels from being cut off
+    plt.savefig('./bandit/images/severity_counts.png', dpi=300)  # Increase DPI
+
+def generate_file_plot(df):
+    # Count the number of issues per file
+    file_counts = df['filename'].value_counts()
+
+    # Plot the number of issues per file (for the top 10 files with the most issues)
+    plt.figure(figsize=(20, 12))  # Make the image larger
+    plt.rcParams.update({'font.size': 18})  # Increase font size
+    sns.barplot(y=file_counts.index[:10], x=file_counts.values[:10], palette=sns.color_palette(["#757575", "#BDBDBD"]), orient='h')  # Two shades of grey
+    plt.title('Number of Issues per File (Top 10)')
+    plt.xlabel('Number of Issues')
+    plt.ylabel('File')
+    plt.tight_layout()  # This will prevent the labels from being cut off
+    plt.savefig('./bandit/images/file_counts.png', dpi=300)  # Increase DPI
+
+def generate_all_plots():
+    # Parse JSON data
+    df = load_and_parse('./reports/bandit-results.json')
+
+    # Generate plots
+    generate_severity_plot(df)
+    generate_file_plot(df)
+
+###########################      MAIN      ###########################
 
 # Parse JSON data to create df
 df = load_and_parse('./reports/bandit-results.json')
