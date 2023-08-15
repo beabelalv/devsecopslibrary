c56e6ca82ddddd1518e252cf2f955036c0a73e70
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-15 18:45:48 +0200

    Changed name error

--- vars/generateSafetyReport.groovy
+++ vars/generateSafetyReport.groovy
@@ -1,12 +1,12 @@
 
 def call(Map config = [:]) {
     // Load the Python script for Safety report generation
-    loadScript(name: 'safety_html_generator.py', path: 'safety/safety_html_generator.py')
+    loadScript(name: 'safety_html_generator.py', path: 'safety/html_generator.py')
     
     // Load the HTML template for Safety report
     def tempTemplateFile = 'temp_safety_report_template.html'
-    loadScript(name: tempTemplateFile, path: 'safety/safety_report_template.html')
+    loadScript(name: tempTemplateFile, path: 'safety/report_template.html')
     
     // Call the Python script with the JSON file and temporary HTML template file
-    sh "python ./safety_html_generator.py ${config.json} ${tempTemplateFile}"
+    sh "python ./html_generator.py ${config.json} ${tempTemplateFile}"
 }
