92e5ead76871022e169bfa60d14f80e6a15fcac2
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-16 17:47:05 +0200

    Sonarqube groovy function changed

--- vars/generateSonarqubeReport.groovy
+++ vars/generateSonarqubeReport.groovy
@@ -1,7 +1,12 @@
-def call() {
+def call(Map config = [:]) {
+    // Default values
+    def issuesJson = config.issuesJson ?: 'sonarqube_open_issues.json'
+    def hotspotsJson = config.hotspotsJson ?: 'sonarqube_open_hotspots.json'
+    def scriptPath = config.scriptPath ?: 'sonarqube/html_generator.py'
+
     // Load the Python script
-    loadScript(name: 'html_generator.py', path: 'sonarqube/html_generator.py')
+    loadScript(name: 'html_generator.py', path: scriptPath)
     
     // Call the Python script with the JSON file paths
-    sh "python ./html_generator.py sonarqube_open_issues.json sonarqube_open_hotspots.json"
+    sh "python ./html_generator.py ${issuesJson} ${hotspotsJson}"
 }
