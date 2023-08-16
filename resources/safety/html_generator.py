d85cf894b2e02610e2c8f833809390a7f3e270af
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-16 13:10:11 +0200

    Edited structure of the code

--- resources/safety/html_generator.py
+++ resources/safety/html_generator.py
@@ -34,7 +34,7 @@ def load_and_parse(file_path):
     df = parse_safety_json(data)
     return df, data
 
-def generate_total_packages_pie_plot(data):
+def generate_packages_summary_plot(data):
     total_packages = len(data['scanned_packages'])
     affected_packages = len(data['affected_packages'])
     safe_packages = total_packages - affected_packages
@@ -42,24 +42,11 @@ def generate_total_packages_pie_plot(data):
     sizes = [affected_packages, safe_packages]
     plt.figure(figsize=(12, 8))
     colors = ['#F57C00', '#66BB6A']
-    plt.pie(sizes, labels=labels, autopct='%1.0f%%', startangle=140, colors=colors)
-    plt.title('Vulnerable vs. Safe Packages', fontsize=15)
+    plt.pie(sizes, labels=labels, autopct='%1.0f%%', labeldistance=1.1, textprops={'fontsize': 14}, startangle=140, colors=colors)
+    plt.title('Summary of Packages', fontsize=20)
     plt.tight_layout()
     plt.gca().xaxis.set_major_formatter(plt.NullFormatter())
-    plt.savefig(os.path.join(images_path, 'total_packages_pie.png'), dpi=300)
-
-def generate_vulnerable_vs_safe_pie_plot(df):
-    vulnerabilities = len(df)
-    safe = len(df[df['advisory'] == 'No known vulnerabilities'])
-    labels = ['Vulnerable', 'Safe']
-    sizes = [vulnerabilities, safe]
-    plt.figure(figsize=(12, 8))
-    colors = ['#FF9999', '#66B2FF']
-    plt.pie(sizes, labels=labels, autopct='%1.0f%%', startangle=140, colors=colors)
-    plt.title('Vulnerable vs. Safe Packages', fontsize=15)
-    plt.tight_layout()
-    plt.gca().xaxis.set_major_formatter(plt.NullFormatter())
-    plt.savefig(os.path.join(images_path, 'vulnerable_vs_safe_pie.png'), dpi=300)
+    plt.savefig(os.path.join(images_path, 'packages_summary_plot.png'), dpi=300)
 
 def generate_vulnerabilities_per_package_plot(df):
     vulnerability_counts = df['package_name'].value_counts()
@@ -70,25 +57,23 @@ def generate_vulnerabilities_per_package_plot(df):
         text.set(color='black')
     for autotext in autotexts:
         autotext.set(color='black', weight='bold')
-    plt.title('Number of Vulnerabilities per Affected Package', fontsize=15)
+    plt.title('Number of Vulnerabilities per Package', fontsize=20)
     plt.tight_layout()
     plt.gca().xaxis.set_major_formatter(plt.NullFormatter())
-    plt.savefig(os.path.join(images_path, 'vulnerabilities_per_package_pie.png'), dpi=300)
+    plt.savefig(os.path.join(images_path, 'vulnerabilities_per_package_plot.png'), dpi=300)
 
-def generate_all_plots_for_safety(file_path):
+def generate_all_plots(file_path):
     df, data = load_and_parse(file_path)
-    generate_total_packages_pie_plot(data)
-    generate_vulnerable_vs_safe_pie_plot(df)
+    generate_packages_summary_plot(data)
     generate_vulnerabilities_per_package_plot(df)
     return df, data
 
 # Generate the plots and get data for the template
-df_safety, safety_data = generate_all_plots_for_safety(file_path)
+df_safety, safety_data = generate_all_plots(file_path)
 
 # Convert the images to data URLs
-total_packages_pie_data_url = get_image_as_data_url(os.path.join(images_path, 'total_packages_pie.png'))
-vulnerable_vs_safe_pie_data_url = get_image_as_data_url(os.path.join(images_path, 'vulnerable_vs_safe_pie.png'))
-vulnerabilities_per_package_pie_data_url = get_image_as_data_url(os.path.join(images_path, 'vulnerabilities_per_package_pie.png'))
+packages_summary_data_url = get_image_as_data_url(os.path.join(images_path, 'packages_summary_plot.png'))
+vulnerabilities_per_package_data_url = get_image_as_data_url(os.path.join(images_path, 'vulnerabilities_per_package_plot.png'))
 
 env = Environment(loader=FileSystemLoader('./'))
 template = env.get_template(template_path)
@@ -96,9 +81,8 @@ template = env.get_template(template_path)
 print("Rendering template...")
 html_content = template.render(
     data=df_safety,
-    total_packages_pie=total_packages_pie_data_url,
-    vulnerable_vs_safe_pie=vulnerable_vs_safe_pie_data_url,
-    vulnerabilities_per_package_pie=vulnerabilities_per_package_pie_data_url
+    total_packages_pie=packages_summary_data_url,
+    vulnerabilities_per_package_pie=vulnerabilities_per_package_data_url
 )
 
 print("Writing HTML content to file...")
