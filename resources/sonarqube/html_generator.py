71fa467d33e3ad8715ea8729efea33f0a74c4501
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-17 12:51:09 +0200

    Changed pink color

--- resources/sonarqube/html_generator.py
+++ resources/sonarqube/html_generator.py
@@ -35,11 +35,11 @@ def load_json(file_path, key):
 def generate_severity_plot(df):
     severity_counts = df['severity'].value_counts()
     colors = {
-        'CRITICAL': '#FF6B6B',
-        'MAJOR': '#FFA36C',
-        'MINOR': '#FFE66D'
+        'CRITICAL': '#e41a1c',  # Red
+        'MAJOR': '#ff7f00',    # Orange
+        'MINOR': '#ffff33'     # Yellow
     }
-    bar_colors = [colors.get(severity, color) for severity, color in zip(severity_counts.index, sns.color_palette("husl", len(severity_counts)))]
+    bar_colors = [colors.get(severity, color) for severity, color in zip(severity_counts.index, sns.color_palette("Paired", len(severity_counts)))]
     
     plt.figure(figsize=(20, 12))
     bars = plt.bar(severity_counts.index, severity_counts.values, color=bar_colors)
@@ -54,7 +54,7 @@ def generate_severity_plot(df):
 def generate_file_plot(df):
     file_counts = df['component'].value_counts().head(10)
     plt.figure(figsize=(20, 12))
-    bars = plt.barh(file_counts.index, file_counts.values, color=sns.color_palette("husl", 10))
+    bars = plt.barh(file_counts.index, file_counts.values, color=sns.color_palette("Paired", 10))
     plt.title('Top 10 Components with Most Issues', fontsize=28)
     plt.xlabel('Number of Issues', fontsize=22)
     plt.ylabel('Components', fontsize=22)
@@ -66,7 +66,7 @@ def generate_file_plot(df):
 def generate_category_plot(df):
     category_counts = df['securityCategory'].value_counts()
     plt.figure(figsize=(20, 12))
-    category_counts.plot.pie(autopct="%.1f%%", colors=sns.color_palette("husl", len(category_counts)), startangle=90, fontsize=22, textprops={'fontsize': 20})
+    category_counts.plot.pie(autopct="%.1f%%", colors=sns.color_palette("Paired", len(category_counts)), startangle=90, fontsize=22, textprops={'fontsize': 20})
     plt.title('Number of Hotspots per Security Category', fontsize=28)
     plt.tight_layout()
     plt.savefig(os.path.join(images_path, 'category_counts.png'))
@@ -74,7 +74,7 @@ def generate_category_plot(df):
 def generate_issue_type_plot(df):
     issue_type_counts = df['type'].value_counts()
     plt.figure(figsize=(20, 12))
-    issue_type_counts.plot.pie(autopct="%.1f%%", colors=sns.color_palette("husl", len(issue_type_counts)), startangle=90, fontsize=22, textprops={'fontsize': 20})
+    issue_type_counts.plot.pie(autopct="%.1f%%", colors=sns.color_palette("Paired", len(issue_type_counts)), startangle=90, fontsize=22, textprops={'fontsize': 20})
     plt.title('Distribution of Issue Types', fontsize=28)
     plt.tight_layout()
     plt.savefig(os.path.join(images_path, 'issue_type_counts.png'))
@@ -82,7 +82,7 @@ def generate_issue_type_plot(df):
 def generate_vulnerability_prob_plot(df):
     vulnerability_prob_counts = df['vulnerabilityProbability'].value_counts()
     plt.figure(figsize=(20, 12))
-    bars = plt.bar(vulnerability_prob_counts.index, vulnerability_prob_counts.values, color=sns.color_palette("husl", len(vulnerability_prob_counts)))
+    bars = plt.bar(vulnerability_prob_counts.index, vulnerability_prob_counts.values, color=sns.color_palette("Paired", len(vulnerability_prob_counts)))
     plt.title('Distribution of Hotspots by Vulnerability Probability', fontsize=28)
     plt.xlabel('Vulnerability Probability', fontsize=22)
     plt.ylabel('Number of Hotspots', fontsize=22)
@@ -94,7 +94,7 @@ def generate_vulnerability_prob_plot(df):
 def generate_hotspot_file_plot(df):
     hotspot_file_counts = df['component'].value_counts().head(10)
     plt.figure(figsize=(20, 12))
-    bars = plt.barh(hotspot_file_counts.index, hotspot_file_counts.values, color=sns.color_palette("husl", 10))
+    bars = plt.barh(hotspot_file_counts.index, hotspot_file_counts.values, color=sns.color_palette("Paired", 10))
     plt.title('Top 10 Components with Most Hotspots', fontsize=28)
     plt.xlabel('Number of Hotspots', fontsize=22)
     plt.ylabel('Components', fontsize=22)
