a727bb67b0fff4398452beb6eb80f5aa715c3d76
beabelalv <beltranalvarezbeatriz@gmail.com>
2023-08-27 23:33:12 +0200

    Edited trufflehog file

--- resources/trufflehog/html_generator.py
+++ resources/trufflehog/html_generator.py
@@ -30,12 +30,12 @@ def load_and_parse_trufflehog(file_path):
     df = pd.DataFrame(data)
     return df
 
-def generate_professional_file_plot_trufflehog_pie(df, top_n=10):
+def generate_file_plot_trufflehog_pie(df, top_n=10):
     file_counts = df['path'].value_counts()
     if len(file_counts) > top_n:
         top_files = file_counts[:top_n]
         others_count = file_counts[top_n:].sum()
-        file_counts = top_files.append(pd.Series(others_count, index=['Others']))
+        file_counts = top_files.append(pd.Series([others_count], index=['Others']))
     colors = sns.color_palette("pastel", len(file_counts))
     plt.figure(figsize=(14, 10))
     wedges, texts, autotexts = plt.pie(file_counts.values, labels=file_counts.index, autopct='%1.1f%%', startangle=140, colors=colors, wedgeprops=dict(width=0.3, edgecolor='w'), pctdistance=0.85, textprops=dict(color="black"))
@@ -52,7 +52,7 @@ def generate_professional_file_plot_trufflehog_pie(df, top_n=10):
 
 # Main logic
 df = load_and_parse_trufflehog(file_path)
-generate_professional_file_plot_trufflehog_pie(df)
+generate_file_plot_trufflehog_pie(df)
 
 # Convert the image to data URL
 file_plot_data_url = get_image_as_data_url(os.path.join(images_path, 'file_counts_trufflehog_pie_professional.png'))
